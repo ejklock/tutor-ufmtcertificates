d7e16f2c6296546a5fff1b3471736f750c95e66b
Evaldo Klock <neto.nemesis@gmail.com>
2020-09-25 15:58:34 -0400

    push to github

---
+++ README.rst
@@ -0,0 +1,22 @@
+ufmtcertificates plugin for `Tutor <https://docs.tutor.overhang.io>`__
+===================================================================================
+
+Installation
+------------
+
+::
+
+    pip install git+https://github.com/ejklock/tutor-ufmtcertificates.git
+
+Usage
+-----
+
+::
+
+    tutor plugins enable ufmtcertificates
+    
+
+License
+-------
+
+This software is licensed under the terms of the AGPLv3.
---
+++ tutorufmtcertificates/plugin.py
@@ -0,0 +1,43 @@
+from glob import glob
+import os
+
+from .__about__ import __version__
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+templates = os.path.join(HERE, "templates")
+
+config = {
+    "add": {
+        "UFMT_CERTIFICATES_MYSQL_USERNAME": "ufmtcertificates",
+        "UFMT_CERTIFICATES_MYSQL_PASSWORD":"ufmtcertificatesonline2020"
+    }
+}
+
+hooks = {
+    "init": ["ufmtcertificates"]
+}
+
+def patches():
+    all_patches = {
+        "local-docker-compose-services":""mysql:
+        ports: "3306:3306""
+    }
+    for path in glob(os.path.join(HERE, "patches", "*")):
+        with open(path) as patch_file:
+            name = os.path.basename(path)
+            content = patch_file.read()
+            all_patches[name] = content
+    return all_patches
+
+def load_all(root):
+    """
+    Return:
+        current (dict): params currently saved in config.yml
+        defaults (dict): default values of params which might be missing from the
+        current config
+    """
+    defaults = load_defaults()
+    current = load_current(root, defaults)
+    return current, defaults
+
